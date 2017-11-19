06aa73252a5feaecec9a9a10b717c4c2dbf05d3b
SnchzMltn <snchzmltn@hotmail.com>
2017-11-19 17:12:18 -0500

    Initial commit

---
+++ Assets/Scripts/loader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loader : MonoBehaviour {
+
+    public GameObject gameManager;
+
+	// Use this for initialization
+	void Awake () {
+    
+        if(GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+		
+	}
+}
